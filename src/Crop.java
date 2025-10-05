8241b89226ad3922f252e7d95f502a3c4f054b50
Mystic <manognachamarti@gmail.com>
2025-10-05 15:56:18 +0100

    Crop object created

--- src/Crop.java
+++ src/Crop.java
@@ -1,12 +1,37 @@
-
 public class Crop{
 
+    public enum Seasons {Spring, Summer, Fall, Winter}
+
     //variables
+    private String cropName;
+    private int stages;
+    private int daysTakenToHarvest;
+    private int seedCost;
+    private int cropSellsFor;
+    private Seasons seasonOfTheCrop;
+
+    //getters and Setters
+    public String getCropName(){return this.cropName;}
+    public int getStages(){return this.stages;}
+    public int getDaysTakenToHarvest(){return this.daysTakenToHarvest;}
+    public int getSeedCost(){return this.seedCost;}
+    public int getCropSellsFor(){return this.cropSellsFor;}
+    public Seasons getSeasonOfTheCrop(){return this.seasonOfTheCrop;}
+
+    
 
     //constructor
-    public Crop(String name){
-        System.out.println(" Fuck YOU i AM " + name);
+    public Crop(String cropName,int stages, int daysTakenToHarvest, int seedCost, int cropSellsFor, Seasons seasonOfTheCrop){
+        this.cropName = cropName;
+        this.stages = stages;
+        this.daysTakenToHarvest = daysTakenToHarvest;
+        this.seedCost = seedCost;
+        this.cropSellsFor = cropSellsFor;
+        this.seasonOfTheCrop = seasonOfTheCrop;
     }
-    //methods
 
+    //methods
+    public void PrintCropDetails(Crop crop){
+        System.out.println(crop.getSeasonOfTheCrop());
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,10 @@ public class Main{
     public static void main(String []args){
 
         //variables
-        Crop myCrop = new Crop("Fuck");
+        Crop myCrop = new Crop("Parsnip",4,4,20,35,Crop.Seasons.Spring);
         //methods
+
+        myCrop.PrintCropDetails(myCrop);
 
     }
 }
