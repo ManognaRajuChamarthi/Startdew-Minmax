457c0331417b8e3d1c1659838a0fc00abcec8efc
Mystic <manognachamarti@gmail.com>
2025-10-06 20:44:14 +0100

    CSV parser Structure

--- src/CSVParser.java
+++ src/CSVParser.java
@@ -1,3 +1,23 @@
+import java.util.*;
+
+
 public class CSVParser {
-    
+    public static List<String[]> ReadCSV(String filePath){
+        List<String[]> rows = new ArrayList<>();
+        
+        try {
+            
+        } 
+        catch (Exception e) {
+        
+        } 
+        
+        return rows;
+    }
+
+    private static List<String> ParseRowOfCSV(String line){
+        List<String> result = new ArrayList<>();
+
+        return result;
+    }
 }
